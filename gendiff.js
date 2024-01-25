0359df97a52281184363eb76a0b743a4cf1333c6
AntonGalygo <anton.galygo@mail.ru>
2024-01-25 15:51:19 +0800

    add arg & option

--- gendiff.js
+++ gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 
-program.name('gendiff').description('Compares two configuration files and shows a difference.').version('0.8.0');
+program.name('gendiff').description('Compares two configuration files and shows a difference.').version('0.8.0').argument('<<filepath1> <filepath2>>');
+program.option('-f, --format [type]', 'output format');
 program.parse();
