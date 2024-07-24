ebff3fe18e5ad02601732b1915856174a2c6bff3
AntonGalygo <anton.galygo@mail.ru>
2024-07-24 12:41:24 +0800

    init

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
-console.log('pops');
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
+
+program.parse();
