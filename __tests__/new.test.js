8c0d09501fbd80a09d81ee4d84ed1eec3e4bf581
AntonGalygo <anton.galygo@mail.ru>
2024-07-26 17:32:49 +0800

    fix test

--- __tests__/new.test.js
+++ __tests__/new.test.js
@@ -9,5 +9,5 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 test('different check', () => {
   const result = '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}';
-  expect(getDifferent(getFixturePath('file1.json'), getFixturePath('file2.json')).toEqual(result));
+  expect(getDifferent(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(result);
 });
