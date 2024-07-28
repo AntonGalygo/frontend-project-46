c9d6fcce30e4817aa97b197dde74603abf9ea488
AntonGalygo <anton.galygo@mail.ru>
2024-07-28 21:39:35 +0800

    step 7

--- src/index.js
+++ src/index.js
@@ -6,6 +6,34 @@ export const getExtension = (filename) => {
   return result.at(-1);
 };
 
+// export const getDifferent = (filepath1, filepath2) => {
+//   const dataFile1 = parser(filepath1, getExtension(filepath1));
+//   const dataFile2 = parser(filepath2, getExtension(filepath2));
+//   const keys1 = Object.keys(dataFile1);
+//   const keys2 = Object.keys(dataFile2);
+//   const keys = _.uniq([...keys1, ...keys2]);
+//   keys.sort();
+//   let result = '';
+//   /* eslint-disable-next-line */
+//   for (const key of keys) {
+//     if (dataFile1[key] === dataFile2[key]) {
+//       result += `  ${key}: ${dataFile1[key]}\n`;
+//     }
+//     if (_.has(dataFile1, key) && !_.has(dataFile2, key)) {
+//       result += `- ${key}: ${dataFile1[key]}\n`;
+//     }
+//     if (!_.has(dataFile1, key) && _.has(dataFile2, key)) {
+//       result += `+ ${key}: ${dataFile2[key]}\n`;
+//     }
+//     if (_.has(dataFile1, key) && _.has(dataFile2, key) && dataFile1[key] !== dataFile2[key]) {
+//       result += `- ${key}: ${dataFile1[key]}\n+ ${key}: ${dataFile2[key]}\n`;
+//     }
+//   }
+//   return `{\n${result}}`;
+// };
+
+const divCount = (div, num) => div.repeat(num);
+
 export const getDifferent = (filepath1, filepath2) => {
   const dataFile1 = parser(filepath1, getExtension(filepath1));
   const dataFile2 = parser(filepath2, getExtension(filepath2));
@@ -14,20 +42,13 @@ export const getDifferent = (filepath1, filepath2) => {
   const keys = _.uniq([...keys1, ...keys2]);
   keys.sort();
   let result = '';
-  /* eslint-disable-next-line */
-  for (const key of keys) {
-    if (dataFile1[key] === dataFile2[key]) {
-      result += `  ${key}: ${dataFile1[key]}\n`;
+  const iter = (data1, data2, deepth) => {
+    if (!_.isObject(obj)) {
+      return `${obj}`;
     }
-    if (_.has(dataFile1, key) && !_.has(dataFile2, key)) {
-      result += `- ${key}: ${dataFile1[key]}\n`;
-    }
-    if (!_.has(dataFile1, key) && _.has(dataFile2, key)) {
-      result += `+ ${key}: ${dataFile2[key]}\n`;
-    }
-    if (_.has(dataFile1, key) && _.has(dataFile2, key) && dataFile1[key] !== dataFile2[key]) {
-      result += `- ${key}: ${dataFile1[key]}\n+ ${key}: ${dataFile2[key]}\n`;
-    }
-  }
+    const lines = Object.entries(obj).map(([key, val]) => `${divCount(divider, deepth)}${key}: ${iter(val, deepth + counter)}`);
+    return ['{', ...lines, `${divCount(divider, deepth - counter)}}`].join('\n');
+  };
+  iter(dataFile1, dataFile2);
   return `{\n${result}}`;
 };
