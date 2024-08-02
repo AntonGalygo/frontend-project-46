1f49d5e47308b748b0813d6978cb3d1057cc5ad0
AntonGalygo <anton.galygo@mail.ru>
2024-08-02 12:36:54 +0800

    step 7

--- src/index.js
+++ src/index.js
@@ -35,20 +35,53 @@ export const getExtension = (filename) => {
 const divCount = (div, num) => div.repeat(num);
 
 export const getDifferent = (filepath1, filepath2) => {
-  const dataFile1 = parser(filepath1, getExtension(filepath1));
-  const dataFile2 = parser(filepath2, getExtension(filepath2));
-  const keys1 = Object.keys(dataFile1);
-  const keys2 = Object.keys(dataFile2);
-  const keys = _.uniq([...keys1, ...keys2]);
-  keys.sort();
-  let result = '';
-  const iter = (data1, data2, deepth) => {
-    if (!_.isObject(obj)) {
-      return `${obj}`;
-    }
-    const lines = Object.entries(obj).map(([key, val]) => `${divCount(divider, deepth)}${key}: ${iter(val, deepth + counter)}`);
-    return ['{', ...lines, `${divCount(divider, deepth - counter)}}`].join('\n');
-  };
-  iter(dataFile1, dataFile2);
-  return `{\n${result}}`;
+  const obj1 = parser(filepath1, getExtension(filepath1));
+  const obj2 = parser(filepath2, getExtension(filepath2));
 };
+
+const getDiffOfObjects = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  const keys = _.sortBy(_.uniq([...keys1, ...keys2]));
+  const result = keys.map((key) => {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+    if (!_.has(obj2, key)) {
+      return {
+        type: 'delete',
+        key,
+        value1,
+      };
+    }
+    if (!_.has(obj1, key)) {
+      return {
+        type: 'add',
+        key,
+        value2,
+      };
+    }
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return {
+        type: 'parent',
+        key,
+        children: getDiffOfObjects(value1, value2),
+      };
+    }
+    if (value1 !== value2) {
+      return {
+        type: 'change',
+        key,
+        value1,
+        value2,
+      };
+    }
+    return {
+      type: 'unchange',
+      key,
+      value1,
+    };
+  });
+  return result;
+};
+// console.log(getDiffOfObjects('__fixtures__/file1.json', '__fixtures__/file2.json'));
+console.log(getDiffOfObjects(parser('__fixtures__/file1.json', getExtension('__fixtures__/file1.json')), parser('__fixtures__/file1.json', getExtension('__fixtures__/file1.json'))));
